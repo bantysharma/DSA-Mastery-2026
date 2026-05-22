4f3b131d6304918c36c784b55b95bfeb7fb6996e
bunty sharma <bantysharma2614@gmail.com>
2026-05-22 23:37:40 +0530

    add one code today

---
+++ 01_Number_System/Example_21.java
@@ -0,0 +1,9 @@
+public class Example_21 {
+    static void greeting(){
+        System.out.println("hello baba world");
+    }
+    public static void main(String[]args) {
+        Example_21.greeting();
+
+    }
+}
