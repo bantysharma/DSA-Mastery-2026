4ae2e5f94297825dc8a5b0b40a60bfc6411825a1
bunty sharma <bantysharma2614@gmail.com>
2026-05-25 23:44:30 +0530

    add one exp

---
+++ 01_Number_System/Example_23.java
@@ -0,0 +1,11 @@
+public class Example_23 {
+    static void Gretting(){
+        System.out.println("hello world");
+    }
+    public static void main(String[]args) {
+        Example_23.Gretting();
+
+
+
+    }
+}
