e4957c5d3a9bbe36a83c62f3cee22a9c77888ff2
bunty sharma <bantysharma2614@gmail.com>
2026-05-26 22:58:55 +0530

    add one example

---
+++ 01_Number_System/Example_24.java
@@ -0,0 +1,8 @@
+public class Example_24 {
+    static void greeting(){
+        System.out.println("hello  kakakak world");
+    }
+    public static void main(String[]args){
+        Example_24.greeting();
+    }
+}
