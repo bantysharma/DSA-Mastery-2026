d29f1f794f2ded948ca896a8d26eaa6df22ff9d9
bunty sharma <bantysharma2614@gmail.com>
2026-05-27 23:17:40 +0530

    one add

---
+++ 01_Number_System/Example_25.java
@@ -0,0 +1,11 @@
+public class Example_25 {
+    static void greetings(){
+        System.out.println("hello honey bunny");
+    }
+
+    public static void main(String[]args) {
+        Example_25.greetings();
+        
+
+    }
+}
