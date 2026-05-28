9ffd7e3d641a7db8450d95e3ede8f596d01dd4b1
bunty sharma <bantysharma2614@gmail.com>
2026-05-28 22:57:10 +0530

    one add task

---
+++ 01_Number_System/Examle_26.java
@@ -0,0 +1,11 @@
+public class Examle_26 {
+    static void greetings(){
+        System.out.println("hello honey bunny");
+    }
+
+    public static void main(String[]args) {
+        Example_25.greetings();
+
+
+    }
+}
