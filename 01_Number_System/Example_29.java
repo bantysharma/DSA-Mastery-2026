0539a705ab1ae401717e8854f32252828de8a634
bunty sharma <bantysharma2614@gmail.com>
2026-05-31 23:55:22 +0530

    one example push

---
+++ 01_Number_System/Example_29.java
@@ -0,0 +1,11 @@
+public class Example_29 {
+    public static  void Greetings(){
+        System.out.println("namo ki bhar bharat");
+    }
+
+    public static void main(String[]args) {
+        Example_29.Greetings();
+
+
+    }
+}
