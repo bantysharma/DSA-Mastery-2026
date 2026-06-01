2466b22ec973154e41d30d60a28f27652bca87c3
bunty sharma <bantysharma2614@gmail.com>
2026-06-01 23:39:04 +0530

    one exaple add

---
+++ 01_Number_System/Example_30.java
@@ -0,0 +1,12 @@
+public class Example_30 {
+
+    public static  void Greetings(){
+        System.out.println("namo ki bhar bharat");
+    }
+
+    public static void main(String[]args) {
+        Example_30.Greetings();
+
+
+    }
+}
